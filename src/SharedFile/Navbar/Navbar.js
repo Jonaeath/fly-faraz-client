feb9d91a0dd8ba68d563b431296bba3d16c95afb
Jonaeath <jonaeathbcc18@gmail.com>
2023-08-29 00:34:18 +0600

    modifyNavbar

--- src/SharedFile/Navbar/Navbar.js
+++ src/SharedFile/Navbar/Navbar.js
@@ -1,3 +1,5 @@
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -10,37 +12,39 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><Link>Item 1</Link></li>
+        <li><Link>Home</Link></li>
+        <li><Link>About</Link></li>
         <li>
-          <Link>Parent</Link>
+          <Link>Services</Link>
           <ul className="p-2">
             <li><Link>Submenu 1</Link></li>
             <li><Link>Submenu 2</Link></li>
           </ul>
         </li>
-        <li><Link>Item 3</Link></li>
+        <li><Link>Contact</Link></li>
       </ul>
     </div>
-    <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
-  </div>
+    <Link className="pl-5 pr-2 text-xl font-bold">FLY Faraz</Link>
+    <FontAwesomeIcon icon={faPlane}/>
+ </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><Link>Item 1</Link></li>
+      <li><Link>Home</Link></li>
+      <li><Link>About</Link></li>
       <li tabIndex={0}>
         <details>
-          <summary>Parent</summary>
+          <summary>Services</summary>
           <ul className="p-2">
             <li><Link>Submenu 1</Link></li>
             <li><Link>Submenu 2</Link></li>
           </ul>
         </details>
       </li>
-      <li><Link>Item 3</Link></li>
+      <li><Link>Contact</Link></li>
     </ul>
   </div>
   <div className="navbar-end">
-    <Link className="btn bg-sky-500">Log In</Link>
-    <Link className="btn bg-sky-500">Sign Up</Link>
+    <Link className="btn bg-sky-500">Log In / Sign Up</Link>
 
   </div>
 </div>
