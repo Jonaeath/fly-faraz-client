f744b7625ce574a3f661df636535a35d5381393b
Jonaeath <jonaeathbcc18@gmail.com>
2023-08-31 12:07:18 +0600

    addLogin & Signup page

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -15,7 +15,9 @@ const Services = () => {
 
     return (
         <div className='ml-9 pb-6 '>
-            <h1>This is service</h1>
+            <h1 className='text-4xl font-bold text-center pb-1'>Services</h1>
+            <h1 className='text-xl font-bold text-center pb-5'>Service Oriented Companies There are many organizations as well as companies who provide service widely in Bangladesh</h1>
+
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             
             {
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../../Layout/MainLayout/MainLayout";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/UserFile/Login";
+import SignUp from "../../Pages/UserFile/SignUp";
 
 export const router = createBrowserRouter([
     {
@@ -9,7 +11,15 @@ export const router = createBrowserRouter([
         children:[
             {
                 path:'/',
-                element:<Home></Home>
+                element:<Home/>
+            },
+            {
+                path:'/login',
+                element:<Login/>
+            },
+            {
+                path:'/signup',
+                element:<SignUp/>
             }
         ]
     }
